0828d7d3351f53a1f6c7a1d59562c64ecd93a804
arya <aapt1720@gmai.com>
2022-01-18 14:24:54 +0330

    clean

--- app.py
+++
@@ -1,6 +0,0 @@
-
-Sum = 0
-for i in range(1, 10+1):
-    Sum += ((1) / (i**2))
-
-print(Sum)
--- loop.py
+++
@@ -1,7 +0,0 @@
-i = 85
-
-while i > 8:
-    while i > 10:
-        print(i+81)
-        i -= 1
-    i -= 1
